132da48269804ef2640a145c7a7a08974b6d18e3
Error Code <122767639+KKBabir7@users.noreply.github.com>
2023-03-21 02:51:38 +0600

    Add files via upload

---
+++ js/slide.js
@@ -0,0 +1,22 @@
+
+		   $('.carousel .vertical .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  for (var i=1;i<4;i++) {
+    next=next.next();
+    if (!next.length) {
+    	next = $(this).siblings(':first');
+  	}
+    
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
+
+$('.carousel').carousel({
+  interval: 3000
+})
+
